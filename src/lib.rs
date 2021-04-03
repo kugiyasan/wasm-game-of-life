11280d7da5e1ccf4175528451e2f41ee5c1d19f8
kugiyasan <kugiyasan@users.noreply.github.com>
2021-04-02 22:01:14 -0400

    :sparkles: play pause button

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,38 @@
 mod utils;
 
+extern crate fixedbitset;
+
+use fixedbitset::FixedBitSet;
 use js_sys::Math;
-use std::fmt;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Cell {
-    Dead = 0,
-    Alive = 1,
-}
-
+/// A Universe is a game of life map
+///
+/// The borders of the map wraps around
+///
+/// A cell is considered alive if its bit is set to true in FixedBitSet
 #[wasm_bindgen]
 pub struct Universe {
     width: u32,
     height: u32,
-    cells: Vec<Cell>,
+    cells: FixedBitSet,
 }
 
 impl Universe {
+    /// Get the dead and alive values of the entire universe
+    pub fn get_cells(&self) -> &FixedBitSet {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
+    }
+
     fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
@@ -50,11 +57,21 @@ impl Universe {
 
 #[wasm_bindgen]
 impl Universe {
-    fn _new<F: Fn(u32) -> Cell>(f: F) -> Self {
+    /// When creating an Universe, this function must be called
+    /// to enable the `console_error_panic_hook` feature
+    fn _new<F: Fn(usize) -> bool>(f: F) -> Self {
+        // Enable the `console_error_panic_hook` feature
+        utils::set_panic_hook();
+
         let width = 64;
         let height = 64;
 
-        let cells = (0..width * height).map(f).collect();
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, f(i));
+        }
 
         Self {
             width,
@@ -63,57 +80,69 @@ impl Universe {
         }
     }
 
+    /// Create a new Universe where every multiple of 2 or 7 is alive
     pub fn new() -> Self {
-        Self::_new(|i| {
-            if i % 2 == 0 || i % 7 == 0 {
-                Cell::Alive
-            } else {
-                Cell::Dead
-            }
-        })
+        Self::_new(|i| i % 2 == 0 || i % 7 == 0)
     }
 
+    /// Create a new Universe with a glider in the top left corner
     pub fn new_with_glider() -> Self {
-        let mut universe = Self::_new(|_| Cell::Dead);
+        let mut universe = Self::_new(|_| false);
 
         let glider: [(u32, u32); 5] = [(0, 1), (1, 2), (2, 0), (2, 1), (2, 2)];
         for &(row, col) in glider.iter() {
             let i = universe.get_index(row, col);
-            universe.cells[i] = Cell::Alive;
+            universe.cells.set(i, true);
         }
 
         universe
     }
 
+    /// Create a new Universe and makes half the cells alive randomly
     pub fn new_random() -> Self {
-        Self::_new(|_| {
-            if Math::random() > 0.5 {
-                Cell::Alive
-            } else {
-                Cell::Dead
-            }
-        })
+        Self::_new(|_| Math::random() > 0.5)
     }
 }
 
 #[wasm_bindgen]
 impl Universe {
+    /// Get the width of the Universe
     pub fn width(&self) -> u32 {
         self.width
     }
 
+    /// Get the height of the Universe
     pub fn height(&self) -> u32 {
         self.height
     }
 
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
+    /// Set the width of the universe
+    ///
+    /// Resets all cells to the dead state
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = FixedBitSet::with_capacity((self.width * self.height) as usize);
     }
 
-    pub fn render(&self) -> String {
-        self.to_string()
+    /// Set the height of the universe
+    ///
+    /// Resets all cells to the dead state
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = FixedBitSet::with_capacity((self.width * self.height) as usize);
     }
 
+    /// Get a raw pointer to the cells
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
+    }
+
+    pub fn toggle_cells(&mut self, row: u32, column: u32) {
+        let idx = self.get_index(row, column);
+        self.cells.toggle(idx);
+    }
+
+    /// Updates the game of life map by one iteration
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
 
@@ -123,40 +152,45 @@ impl Universe {
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
 
+                log!(
+                    "cell[{}, {}] is initially {:?} and has {} live neighbors",
+                    row,
+                    col,
+                    cell,
+                    live_neighbors,
+                );
+
                 let next_cell = match (cell, live_neighbors) {
                     // Rule 1: Any live cell with fewer than two live neighbours
                     // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (true, x) if x < 2 => false,
                     // Rule 2: Any live cell with two or three live neighbours
                     // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (true, 2) | (true, 3) => true,
                     // Rule 3: Any live cell with more than three live
                     // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (true, x) if x > 3 => false,
                     // Rule 4: Any dead cell with exactly three live neighbours
                     // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
+                    (false, 3) => true,
                     // All other cells remain in the same state.
                     (otherwise, _) => otherwise,
                 };
 
-                next[idx] = next_cell;
+                log!("    it becomes {:?}", next_cell);
+
+                if next[idx] != next_cell {
+                    log!(
+                        "    The state of the cell changed from {} to {}",
+                        next[idx],
+                        next_cell
+                    );
+                }
+
+                next.set(idx, next_cell);
             }
         }
 
         self.cells = next;
-    }
-}
-
-impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
-            }
-            write!(f, "\n")?;
-        }
-        Ok(())
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+extern crate web_sys;
+
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
@@ -7,4 +9,29 @@ pub fn set_panic_hook() {
     // https://github.com/rustwasm/console_error_panic_hook#readme
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
+}
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+/// A macro to provide `println!(..)`-style syntax for `console.log` logging
+///
+/// BE SURE to compile your code with `debug = true`
+/// or else this macro won't do anything
+#[macro_export]
+#[cfg(debug_assertions)]
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
+/// When not in debug mode, `log` won't do anything
+#[macro_export]
+#[cfg(not(debug_assertions))]
+macro_rules! log {
+    ( $( $t:tt )* ) => {}
 }
